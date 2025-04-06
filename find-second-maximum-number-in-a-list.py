3bd926c8f96b08ceeb891edb0df1f043c3df2003
just-scribblig <hailemariamlibassie55@gmail.com>
2025-04-06 03:32:38 +0300

    Update find-second-maximum-number-in-a-list.py

--- find-second-maximum-number-in-a-list.py
+++ find-second-maximum-number-in-a-list.py
@@ -1,13 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
     nums = list(set(map(int, input().split())))
+    nums.sort()
     n = len(nums)
-    for i in range(n):
-        swap = 0
-        for j in range(i, n-1):
-            if nums[j] > nums[j+1]:
-                nums[j], nums[j+1] = nums[j+1], nums[j]
-                swap += 1
-        if swap == 0:
-            break
-    print(nums[n-2] if len(nums) > 2 else None)
+    print(nums[n-2] if len(nums) >= 2 else None)
